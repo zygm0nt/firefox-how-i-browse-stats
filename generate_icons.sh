c64ddd30ee39fc8fd9a6feef27b3559198e98a6f
Marcin Cylke <marcin.cylke+github@gmail.com>
2026-01-05 17:33:23 +0100

    extension icon

---
+++ generate_icons.sh
@@ -0,0 +1,6 @@
+magick icons/icon-original.png \
+-interpolate Nearest -filter point \
+-resize 128x128 -write icons/icon-128.png \
+-resize 48x48   -write icons/icon-48.png \
+-resize 32x32   -write icons/icon-32.png \
+-resize 16x16          icons/icon-16.png
